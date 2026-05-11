3394a7f8cd5e8b57607c8787ff55b83dbd89d881
ketlynmelo <ketlyn_de-melo@estudante.sesisenai.org.br>
2026-05-11 08:32:55 -0300

    Fix - Arrumei um erro na tela de login

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ let form = document.getElementById('formulario');
 
 let dados = [];
 
+let toggle = document.getElementById("toggle");
+
+toggle.addEventListener("click", function () {
+
+    alert("Pedido enviado ao Administrador");
+
+});
+
 form.addEventListener('submit', function (e) {
 
     e.preventDefault();
@@ -10,9 +18,43 @@ form.addEventListener('submit', function (e) {
 
     let usuario = document.getElementById('usuario').value;
     let senha = document.getElementById('senha').value;
+    let mensagem = document.getElementById("mensagem");
 
-    if (usuario.trim() === '') {   //campo vazio
-        alert("Campo de usuário vazio!");
+    mensagem.innerHTML = "";
+
+   
+    if (senha.trim() === '') {
+      mensagem.innerHTML = `
+        <div class='erro'>
+        <p>Campo de senha vazio!</p>
+        </div>
+        `; 
+        valido = false;
+    } else if (senha.trim() === '1532') { //verificar se a senha é a correta//
+
+    } else {
+    mensagem.innerHTML = `
+        <div class='erro'>
+        <p>Senha incorreta!</p>
+        </div>
+        `; 
+        valido = false;
+    }
+     if (usuario.trim() === '') {   //campo vazio
+        mensagem.innerHTML = `
+        <div class='erro'>
+        <p>Campo de usuário vazio!</p>
+        </div>
+        `; 
+        valido = false;
+    } else if (usuario.trim() === 'Administrador') { //Verificar o nome//
+
+    } else {
+        mensagem.innerHTML = `
+        <div class='erro'>
+        <p>Usuário incorreto!</p>
+        </div>
+        `; 
         valido = false;
     }
 
@@ -28,7 +70,7 @@ form.addEventListener('submit', function (e) {
         dados.push(usuarioObj);
         console.table(dados);
         alert('Formulário enviado com sucesso!');
-        window.location.href = "pag_inicial.html";
+        window.location.href = "home_historia.html";
 
         form.reset();
     }
